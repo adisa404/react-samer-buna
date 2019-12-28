ebd2684962b354ab6041a0c84300d6dcb5c265cc
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2019-12-28 22:16:21 +0100

    project github cards - add Card markup.

--- github-cards/src/components/Card.jsx
+++ github-cards/src/components/Card.jsx
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 class Card extends Component {
   state = {};
   render() {
-    return <div className="github-profile">One Github Profile</div>;
+    return (
+      <div className="github-profile">
+        <img src="https://placehold.it/100" />
+        <div className="info">
+          <div className="name">Name ...</div>
+          <div className="company">Company ...</div>
+        </div>
+      </div>
+    );
   }
 }
 
