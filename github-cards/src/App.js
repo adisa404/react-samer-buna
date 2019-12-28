1f216f5847e07f810eca8fa501ddec3b48cf542f
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2019-12-29 00:33:22 +0100

    project github cards - make testData available in state of App.

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -3,31 +3,32 @@ import CardList from './components/CardList';
 import Form from './components/Form';
 import './App.scss';
 
+const testData = [
+  {
+    name: 'Dan Abramov',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4',
+    company: '@facebook',
+  },
+  {
+    name: 'Sophie Alpert',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/6820?v=4',
+    company: 'Humu',
+  },
+  {
+    name: 'Sebastian Markbåge',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/63648?v=4',
+    company: 'Facebook',
+  },
+];
+
 class App extends Component {
-  state = {};
+  state = { profiles: testData };
   render() {
-    const testData = [
-      {
-        name: 'Dan Abramov',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4',
-        company: '@facebook',
-      },
-      {
-        name: 'Sophie Alpert',
-        avatar_url: 'https://avatars2.githubusercontent.com/u/6820?v=4',
-        company: 'Humu',
-      },
-      {
-        name: 'Sebastian Markbåge',
-        avatar_url: 'https://avatars2.githubusercontent.com/u/63648?v=4',
-        company: 'Facebook',
-      },
-    ];
     return (
       <div>
         <div className="header">{this.props.title}</div>
         <Form />
-        <CardList profiles={testData} />
+        <CardList profiles={this.state.profiles} />
       </div>
     );
   }
